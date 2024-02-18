c2b94b3899186665969a1851a0d55a393f1f61cf
Aleksey Semenov <ceratogyrus35@gmail.com>
2024-02-18 10:25:49 +0300

    Added Task01_Sem1

---
+++ lesson1/task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 105;
+int b = 2;
+int c = 7;
+int d = 15;
+int e = 2;
+
+int max = a;
+
+if (b>max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;
+}
+if (d>max)
+{
+    max = d;
+}
+if (e>max)
+{
+    max = e;
+}
+Console.WriteLine(max);
